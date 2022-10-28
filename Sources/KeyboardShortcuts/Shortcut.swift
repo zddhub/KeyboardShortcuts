4151158c2411b07d99527bba32f83d5b66e8d725
zdd <zddhub@gmail.com>
2022-10-28 22:16:51 +0800

    Add as SwiftUI .keyboardShortcut() helper
    Copy from https://github.com/sindresorhus/KeyboardShortcuts/pull/69/files

--- Sources/KeyboardShortcuts/Shortcut.swift
+++ Sources/KeyboardShortcuts/Shortcut.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import Carbon.HIToolbox
+import SwiftUI
 
 extension KeyboardShortcuts {
 	/**
@@ -299,4 +300,61 @@ extension KeyboardShortcuts.Shortcut: CustomStringConvertible {
 	public var description: String {
 		modifiers.description + (keyToCharacter()?.uppercased() ?? "�")
 	}
+}
+
+// MARK: - SwiftUI Helpers
+
+extension KeyboardShortcuts.Shortcut {
+
+  @available(iOS 14.0, macOS 11.0, *)
+  var swiftUI: SwiftUI.KeyboardShortcut? {
+    guard let key = keyEquivalent.first else { return nil }
+    return .init(.init(key), modifiers: modifiers.swiftUI)
+  }
+
+}
+
+@available(macOS 10.15, *)
+extension NSEvent.ModifierFlags {
+  var swiftUI: SwiftUI.EventModifiers {
+    var modifiers: SwiftUI.EventModifiers = []
+    if contains(.shift) {
+      modifiers.insert(.shift)
+    }
+    if contains(.command) {
+      modifiers.insert(.command)
+    }
+    if contains(.capsLock) {
+      modifiers.insert(.capsLock)
+    }
+    if contains(.function) {
+      modifiers.insert(.function)
+    }
+    if contains(.option) {
+      modifiers.insert(.option)
+    }
+    if contains(.control) {
+      modifiers.insert(.control)
+    }
+    return modifiers
+  }
+}
+
+@available(iOS 14.0, macOS 11.0, *)
+extension View {
+
+  @ViewBuilder
+  /// Assigns the global keyboard shortcut to the modified control.
+  ///
+  /// Only assigns a keyboard shortcut, if one was defined (or it has a default shortcut).
+  ///
+  /// - Parameter shortcut: Strongly-typed name of the shortcut
+  public func keyboardShortcut(_ shortcut: KeyboardShortcuts.Name) -> some View {
+    if let shortcut = (shortcut.shortcut ?? shortcut.defaultShortcut)?.swiftUI {
+      self.keyboardShortcut(shortcut)
+    } else {
+      self
+    }
+  }
+
 }
