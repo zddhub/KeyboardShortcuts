0864b660ad44a3bd2fdaef0aa9886c946e8a785a
zdd <zddhub@gmail.com>
2022-10-28 22:16:51 +0800

    Add as SwiftUI .keyboardShortcut() helper
    Copy from https://github.com/sindresorhus/KeyboardShortcuts/pull/69/files

--- Sources/KeyboardShortcuts/Shortcut.swift
+++ Sources/KeyboardShortcuts/Shortcut.swift
@@ -778,4 +778,63 @@ extension KeyboardShortcuts.Shortcut {
 		return nil
 	}
 }
+
+// MARK: - SwiftUI Helpers
+
+extension KeyboardShortcuts.Shortcut {
+
+  @available(iOS 14.0, macOS 11.0, *)
+  @MainActor
+  var swiftUI: SwiftUI.KeyboardShortcut? {
+    guard let key = keyEquivalent.first else { return nil }
+    return .init(.init(key), modifiers: modifiers.swiftUI)
+  }
+
+}
+
+@available(macOS 10.15, *)
+extension NSEvent.ModifierFlags {
+  var swiftUI: SwiftUI.EventModifiers {
+    var modifiers: SwiftUI.EventModifiers = []
+    if contains(.shift) {
+      modifiers.insert(.shift)
+    }
+    if contains(.command) {
+      modifiers.insert(.command)
+    }
+    if contains(.capsLock) {
+      modifiers.insert(.capsLock)
+    }
+    if contains(.function) {
+      modifiers.insert(.function)
+    }
+    if contains(.option) {
+      modifiers.insert(.option)
+    }
+    if contains(.control) {
+      modifiers.insert(.control)
+    }
+    return modifiers
+  }
+}
+
+@available(iOS 14.0, macOS 11.0, *)
+extension View {
+
+  @ViewBuilder
+  /// Assigns the global keyboard shortcut to the modified control.
+  ///
+  /// Only assigns a keyboard shortcut, if one was defined (or it has a default shortcut).
+  ///
+  /// - Parameter shortcut: Strongly-typed name of the shortcut
+  @MainActor
+  public func keyboardShortcut(_ shortcut: KeyboardShortcuts.Name) -> some View {
+    if let shortcut = (shortcut.shortcut ?? shortcut.defaultShortcut)?.swiftUI {
+      self.keyboardShortcut(shortcut)
+    } else {
+      self
+    }
+  }
+
+}
 #endif
